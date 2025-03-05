32a5c24426bf6cc540a68a995900eb878682e3bf
dima cheban <dimacheban23@gmail.com>
2025-03-05 14:14:03 +0200

    create token handler

---
+++ src/services/token_handler/strategies/__init__.py
@@ -0,0 +1,6 @@
+from src.services.token_handler.strategies.access_token import (
+    AccessTokenStrategy
+)
+from src.services.token_handler.strategies.refresh_token import (
+    RefreshTokenStrategy
+)
---
+++ src/services/token_handler/strategies/refresh_token.py
@@ -0,0 +1,16 @@
+from src.services.token_handler.strategies.base_strategy import (
+    ITokenStrategy
+)
+from datetime import timedelta
+
+
+class RefreshTokenStrategy(ITokenStrategy):
+
+    def _get_default_expiry(self) -> timedelta:
+        return timedelta(days=1)
+
+    async def create_token(self, data, expire_delta=None) -> str:
+        return self._encode_token(data, "refresh_token", expire_delta)
+
+    async def decode_token(self, token) -> dict:
+        return self._decode_token(token, "refresh_token")
